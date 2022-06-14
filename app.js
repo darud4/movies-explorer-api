4c5fc52b3dfbe4ba7400ad8ae5001ce3b780864b
Pavel Bezrukov <darud4@yandex.ru>
2022-06-14 23:51:58 +0400

    feat: do some mess

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { celebrate } = require('celebrate');
+
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const cors = require('cors');
@@ -7,6 +9,11 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { errorHandler } = require('./utils/errorHandler');
 const { CONFIG } = require('./config');
 const router = require('./routes/index');
+const { NotFound } = require('./errors/NotFound');
+const { loginValidation, registerValidation } = require('./utils/joiValidators');
+const { login, createUser } = require('./controllers/users');
+const { checkToken } = require('./middlewares/auth');
+
 
 const CORS_CONFIG = {
   credentials: true,
@@ -19,12 +26,21 @@ const app = express();
 
 // TODO Helmet
 // TODO Rate limiter
-
 app.use(cors(CORS_CONFIG));
 app.use(bodyParser.json({ extended: true }));
 app.use(requestLogger);
 
-app.use(router);
+// app.use(router);
+
+app.post('/signin', celebrate(loginValidation), login);
+app.post('/signup', celebrate(registerValidation), createUser);
+
+app.use(checkToken);
+
+app.use((req, res, next) => {
+  console.log(404);
+  next(new NotFound('Запрошенной страницы не существует'));
+});
 
 app.use(errorLogger);
 app.use(errors());
